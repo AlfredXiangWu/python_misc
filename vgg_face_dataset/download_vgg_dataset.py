e1a24cb50929a70fc207016ebb3011caa4b7fa76
AlfredXiangWu <alfredxiangwu@gmail.com>
2016-03-07 13:42:39 +0800

    add multiprocessing

--- vgg_face_dataset/download_vgg_dataset.py
+++ vgg_face_dataset/download_vgg_dataset.py
@@ -1,9 +1,12 @@
 import os
 import urllib2
+import httplib
+
+import multiprocessing
 
 def download_image(url, save_name):
 	try:
-		fp = urllib2.urlopen(url, timeout=60)
+		fp = urllib2.urlopen(url, timeout=20)
 		data = fp.read()
 		fp.close()
 
@@ -11,12 +14,14 @@ def download_image(url, save_name):
 		fid = open(save_name, 'w+b')
 		fid.write(data)
 		flag = True 
-	except IOError, HTTPError:
-		print url + ' downloading error...'
+	except Exception:
+		print url + ' downloading io error...'
 		flag = False
 	return flag
 
-def readList(list_path, save_path, log_path):
+
+
+def readList((list_path, save_path, log_path)):
 	if not os.path.exists(save_path):
 		os.mkdir(save_path)
 
@@ -29,9 +34,15 @@ def readList(list_path, save_path, log_path):
 		name = tmp[0]
 		url = tmp[2]
 		filename = save_path + name + '.jpg'
+
+		# skip the existing images
+		if os.path.exists(filename):
+			print filename + ' is already downloaded..'
+			continue
+
 		flag = download_image(url, filename)
 		if not flag:
-			fid_log.write(line + '\n')
+			fid_log.write(line)
 			#raw_input();
 		line = fid.readline()
 
@@ -41,15 +52,21 @@ def readList(list_path, save_path, log_path):
 
 def readPath(list_dir, save_dir, log_dir):
 	count = 0;
+	tasks = []
 	for root, dirs, files in os.walk(list_dir):
 		for fn in files:
 			folder = fn.replace('.txt', '')
-			log_path = log_dir + folder + '.txt';
+			log_path = log_dir + folder + '_error.txt';
 			save_path = save_dir + folder + '/'
-			print root + fn
-			print save_path
-			readList(root + fn, save_path, log_path)
-
+			
+			tasks.append((root + fn, save_path, log_path))
+			#readList(root + fn, save_path, log_path)
+	pool_size = multiprocessing.cpu_count()
+	pool = multiprocessing.Pool(processes=pool_size, maxtasksperchild=2)
+	pool.map(readList, tasks)
+	pool.close()
+	pool.join()
+	
 
 
 if __name__ == "__main__":
